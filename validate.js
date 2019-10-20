147202372337b416c3e4ae9d1d19e197624c2a51
Alistair Blake <consultations@me.com>
2019-10-20 13:07:16 -0700

    added tests to support the other CRUD Methods --delete() --update() and corrected linter errors

--- validate.js
+++ validate.js
@@ -1,15 +1,15 @@
-'use strict';
-
-class Validator {
-  String (input) {
-    return typeof input === 'string';
-    };
-  Number (input) {
-    return typeof input === 'number';
-  };
-  Boolean = (input) => {
-    return typeof input === 'boolean';
-  };
-}
-
-modlue.exports = Validator;
+// 'use strict';
+//
+// class Validator {
+//   String (input) {
+//     return typeof input === 'string';
+//     };
+//   Number (input) {
+//     return typeof input === 'number';
+//   };
+//   Boolean = (input) => {
+//     return typeof input === 'boolean';
+//   };
+// }
+//
+// modlue.exports = Validator;
